23689dc22fc9bdbc7326c5f62ba2c9f53a6a252d
sdt5106 <sdt5106@206-L70.up.ist.local>
2015-10-28 19:37:55 -0400

    home panel

---
+++ src/brick2/Home.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brick2;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author sdt5106
+ */
+public class Home extends JPanel {
+    
+    Home(){
+        super();
+    }
+}
