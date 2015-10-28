db48177f253d578391594011b3726eef36b0666c
sdt5106 <sdt5106@206-L70.up.ist.local>
2015-10-28 19:42:52 -0400

    home panel

--- src/brick2/Brick2.java
+++ src/brick2/Brick2.java
@@ -16,6 +16,7 @@ public class Brick2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        myJFrame f1 = new myJFrame();
     }
     
 }
--- src/brick2/Home.java
+++ src/brick2/Home.java
@@ -5,6 +5,7 @@
  */
 package brick2;
 
+import java.awt.GridLayout;
 import javax.swing.*;
 
 /**
@@ -12,8 +13,18 @@ import javax.swing.*;
  * @author sdt5106
  */
 public class Home extends JPanel {
+
+    JButton b1, b2, b3;
     
-    Home(){
+    Home() {
         super();
+        setLayout(new GridLayout(3,1));
+        b1 = new JButton("Play");
+        b2 = new JButton("High score");
+        b3 = new JButton("Exit");
+        add(b1);
+        add(b2);
+        add(b3);
+        
     }
 }
