145c9c33faf8417dee5d8522e706e55839506366
sdt5106 <sdt5106@206-L70.up.ist.local>
2015-10-28 20:38:10 -0400

    game stuff

--- src/brick2/Play.java
+++ src/brick2/Play.java
@@ -5,21 +5,30 @@
  */
 package brick2;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+
+import java.awt.event.KeyListener;
 import javax.swing.*;
 
 /**
  *
  * @author sdt5106
  */
-public class Play extends JPanel  {
+public class Play extends JPanel implements KeyListener {
 
+    PaddleClass paddle;
+    Graphics g;
     static int stageno, scoreno, livesno;
 
     JLabel stage, score, lives;
 
     Play() {
         super();
-
+        setFocusable(true);
+        
         setLayout(null);
         init();
     }
@@ -36,15 +45,52 @@ public class Play extends JPanel  {
         add(stage);
         add(score);
         add(lives);
-        
-        stage.setBounds(5,5, 100, 25);
+
+        stage.setBounds(5, 5, 100, 25);
         score.setBounds(115, 5, 100, 25);
         lives.setBounds(225, 5, 100, 25);
+
+        paddle = new PaddleClass();
+
+        addKeyListener(this);
+
     }
-    
-    void updatescore(){
+
+    void updatescore() {
         scoreno += 10;
         score.setText("Score: " + scoreno);
     }
 
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        requestFocusInWindow();
+        g.fillRect(paddle.xPos, paddle.Y_POS, paddle.PADDLE_WIDTH, paddle.PADDLE_HEIGHT);
+        g.setColor(Color.pink);
+        g.drawRect(paddle.xPos, paddle.Y_POS, paddle.PADDLE_WIDTH, paddle.PADDLE_HEIGHT);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        int x = ke.getKeyCode();
+        if (x == KeyEvent.VK_LEFT) {
+            System.out.println("test");
+            int temp = paddle.getxpos();
+            if (temp > 0) {
+                paddle.setxpos(-25);
+                revalidate();
+                repaint();
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+
+    }
 }
